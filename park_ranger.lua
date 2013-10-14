9fdc8fb5245ecca1b93486bf8deb1d42966ab7ba
Adam Hope <ahope@thoughtworks.com>
2013-10-14 19:13:44 +1100

    start of park ranger

---
+++ park_ranger.lua
@@ -0,0 +1,49 @@
+require 'entity'
+
+ParkRanger = {}
+ParkRanger.__index = ParkRanger
+setmetatable(ParkRanger, {__index = Entity})
+
+function ParkRanger:new(game)
+
+    local newParkRanger = Entity:new(game)
+
+    newParkRanger.graphics = {
+        source = "assets/images/park_ranger.png"
+    }
+    newParkRanger.size = {
+        x = 80,
+        y = 114
+    }
+    newParkRanger.x = 0
+
+    newParkRanger.type = "park_ranger"
+    -- newParkRanger.x = ScreenWidth
+    newParkRanger.y = ScreenHeight - newParkRanger.size.y -- - GroundYOffset
+
+
+    if game.graphics ~= nil and game.animation ~= nil then
+        newParkRanger.graphics.sprites = game.graphics.newImage(newParkRanger.graphics.source)
+        newParkRanger.graphics.grid = game.animation.newGrid(
+            newParkRanger.size.x, newParkRanger.size.y,
+            newParkRanger.graphics.sprites:getWidth(),
+            newParkRanger.graphics.sprites:getHeight()
+        )
+        newParkRanger.graphics.animation = game.animation.newAnimation(
+            newParkRanger.graphics.grid("1-1", 1),
+            0.05
+        )
+    end
+
+    return setmetatable(newParkRanger, self)
+end
+
+function ParkRanger:update(dt)
+
+    -- self.x = self.x - CameraXSpeed
+
+    if self.graphics.animation ~= nil then
+        self.graphics.animation:update(dt)
+    end
+
+end
---
+++ spec/park_ranger_spec.lua
@@ -0,0 +1,29 @@
+require 'park_ranger'
+require 'entity'
+require 'conf'
+require 'spec/mock_spec'
+
+describe("Park ranger", function()
+    describe("new park ranger",function()
+
+        it("the park ranger should be on the floor", function()
+            local park_ranger  = ParkRanger:new(mock_game())
+            assert.is.equal(park_ranger.y, ScreenHeight - park_ranger.size.y)
+        end)
+
+        it("should be on the left edge of the screen", function()
+            local park_ranger  = ParkRanger:new(mock_game())
+            assert.is.equal(park_ranger.x, 0)
+        end)
+
+
+
+    it("park_ranger should have size and image passed through image spec", function()
+            local park_ranger = ParkRanger:new(mock_game())
+            assert.is.equal("assets/images/park_ranger.png", park_ranger.graphics.source)
+            assert.is.equal(80, park_ranger.size.x)
+            assert.is.equal(114, park_ranger.size.y)
+        end)
+
+    end)
+end)
